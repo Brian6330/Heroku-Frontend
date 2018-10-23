16686eb670660face2ad89488aff68038cb37ce8
Brian6330 <brian@schweigler.org>
2018-10-23 08:45:08 +0200

    Trying to implement a "to Top" button
    WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,8 @@ export class AppComponent implements OnInit {
 	constructor(private httpClient: HttpClient) {
    // this.baseUrl = environment.baseUrl;
 	}
+	
+	
 
 	ngOnInit() {
 		this.httpClient.get(this.baseUrl + '/joblist').subscribe((instances: any) => {
@@ -37,6 +39,23 @@ export class AppComponent implements OnInit {
 
 	onJobListDestroy(jobList: JobList) {
 		this.jobLists.splice(this.jobLists.indexOf(jobList), 1);
+	}
+	
+	// When the user scrolls down 10px from the top of the document, show the button
+	window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+		if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+			document.getElementById("myBtn").style.display = "block";
+		} else {
+			document.getElementById("myBtn").style.display = "none";
+		}
+	}
+
+	// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+		document.body.scrollTop = 0; // For Safari
+		document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 	}
 
 }
